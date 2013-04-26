aec7382bc261caadb14cf556ab875ad01b404cb0
kkammone <kari.kammonen@gofore.com>
2013-04-26 15:13:06 +0300

    siistitty koodia

--- src/main/java/fi/vm/sade/valintaperusteet/config/ConfigController.java
+++ src/main/java/fi/vm/sade/valintaperusteet/config/ConfigController.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.servlet.http.HttpServletResponse;
+
 
 /**
- * User: tommiha
- * Date: 2/15/13
- * Time: 2:43 PM
+ * @author kkammone
  */
 @Controller
 public class ConfigController {
@@ -20,27 +18,12 @@ public class ConfigController {
 
     @RequestMapping(value="/configuration.js", method = RequestMethod.GET, produces="text/javascript")
     @ResponseBody
-    public String index(HttpServletResponse response) {
-       // response.setContentType("text/javascript");
-      //  response.setCharacterEncoding("UTF-8");
+    public String index() {
         StringBuilder b = new StringBuilder();
         append(b, "SERVICE_URL_BASE", valintaperusteServiceRestURL);
         append(b, "TEMPLATE_URL_BASE", "");
-        /*
-        try{
-            PrintWriter out = response.getWriter();
-            out.println(b.toString());
-            out.close();
-        }
-        catch(IOException ex){
-          ex.printStackTrace();
-        }
-            //response.getWriter().print(b.toString());
-            //return b.toString();
-            */
         return b.toString();
-
-        }
+    }
 
     private void append(StringBuilder b, String key, String value) {
         b.append(key);
