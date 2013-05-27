7e696d62d527431a3e886164756d2ad8be1f3c17
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2013-05-27 09:53:45 +0300

    tarjonnan synkronointi näkymän korjaus

--- src/main/webapp/js/import.js
+++ src/main/webapp/js/import.js
@@ -6,29 +6,21 @@ app.factory('HakuModel', function(Haku, $http) {
         this.haut = [];
 
         this.init = function(oid) {
-
-            if(model.haut.length <= 0) {
-                $http.get(TARJONTA_URL_BASE + "haku").success(function(result) {
-                    model.haut = result;
-                    model.hakuOid = model.haut[0];
-                });
-            }
-
-
-            // Ladataan haut vain kerran. Haut tuskin muuttuvat kovinkaan usein.
-            /*
+            
             if(model.haut.length <= 0) {
                 Haku.get({}, function(result) {
                     model.haut = result;
-                    model.hakuOid = model.haut[0];
+                    model.hakuOid = model.haut[0].oid;
+
+                    //set haku (in view) to hakuselect to what is was or to first option
                     model.haut.forEach(function(haku){
                         if(haku.oid == oid) {
                             model.hakuOid = haku;
                         }
                     });
                 });
+
             }
-            */
         }
     };
 
@@ -40,7 +32,8 @@ function ImportController($scope, $location, $routeParams, HakuModel, TarjontaIm
     HakuModel.init($routeParams.hakuOid);
 
     $scope.aktivoi = function() {
-        TarjontaImport.aktivoi({hakuOid: $scope.model.hakuOid}, function() {  });
+        TarjontaImport.aktivoi({hakuOid: $scope.model.hakuOid.oid}, function() {  });
     };
 
 }
+
