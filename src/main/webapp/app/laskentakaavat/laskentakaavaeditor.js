61dd4a341e74d230740aee3de1c7f5eb2acbf7f3
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-03-26 09:33:52 +0200

    murupolku laskentakaavaeditoriin joka sivulle & käyttöliittymän yhtenäistämistä

--- src/main/webapp/app/laskentakaavat/laskentakaavaeditor.js
+++ src/main/webapp/app/laskentakaavat/laskentakaavaeditor.js
@@ -11,9 +11,9 @@ angular.module('LaskentakaavaEditor').config(function($routeProvider) {
 	$routeProvider.
 	when('/laskentakaava', {controller: 'LaskentakaavaListController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/listaus/laskentakaavalista.html', label: 'laskentakaavalista'}).
 	when('/laskentakaava/:laskentakaavaOid', {controller: 'LaskentakaavaController', templateUrl:TEMPLATE_URL_BASE + 'laskentakaavat/laskentakaavapuu/laskentakaavalomake.html', label: 'laskentakaava'}).
-	when('/valintaryhma/:valintaryhmaOid/laskentakaava', {controller: 'LaskentakaavaListController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/listaus/laskentakaavalista.html', label: 'laskentakaava'}).
+	when('/valintaryhma/:valintaryhmaOid/laskentakaava', {controller: 'LaskentakaavaListController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/listaus/laskentakaavalista.html', label: 'laskentakaavalista'}).
     when('/valintaryhma/:valintaryhmaOid/laskentakaava/:laskentakaavaOid', {controller: 'LaskentakaavaController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/laskentakaavapuu/laskentakaavalomake.html', label: 'laskentakaava'}).
-    when('/hakukohde/:hakukohdeOid/laskentakaava', {controller: 'HakukohdeLaskentakaavaListController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/listaus/hakukohdelaskentakaavalista.html', label: 'laskentakaava'}).
+    when('/hakukohde/:hakukohdeOid/laskentakaava', {controller: 'HakukohdeLaskentakaavaListController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/listaus/hakukohdelaskentakaavalista.html', label: 'laskentakaavalista'}).
     when('/hakukohde/:hakukohdeOid/laskentakaava/:laskentakaavaOid', {controller: 'LaskentakaavaController', templateUrl: TEMPLATE_URL_BASE + 'laskentakaavat/laskentakaavapuu/laskentakaavalomake.html', label: 'laskentakaava'})
 });
 
