b5b8970f3185e300c9436f358f7b20702a261b8c
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-05-13 14:39:20 +0300

    VT-842

--- src/test/ui/unit/LaskentakaavaControllerTest.js
+++ src/test/ui/unit/LaskentakaavaControllerTest.js
@@ -31,20 +31,38 @@ describe('LaskentakaavaController', function () {
             return $controller('LaskentakaavaController', {$scope: scope});
         }
     }));
-/*
-    afterEach(function() {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
-    });
-*/
+    /*
+     afterEach(function() {
+     $httpBackend.verifyNoOutstandingExpectation();
+     $httpBackend.verifyNoOutstandingRequest();
+     });
+     */
     describe('laskentakaavaController', function () {
 
-        beforeEach(function() {
-            var funktiokutsu = {"lapsi":{"funktionimi":"KESKIARVO","arvokonvertteriparametrit":[],"arvovalikonvertteriparametrit":[],"syoteparametrit":[],"funktioargumentit":[{"lapsi":{"funktionimi":"HAELUKUARVO","arvokonvertteriparametrit":[],"arvovalikonvertteriparametrit":[],"syoteparametrit":[],"funktioargumentit":[],"valintaperusteviitteet":[{"tunniste":"PK_A1","kuvaus":"","lahde":"HAETTAVA_ARVO","onPakollinen":false,"epasuoraViittaus":false,"indeksi":2}],"validointivirheet":[],"onLuonnos":null,"nimi":null,"kuvaus":null,"tyyppi":null,"id":819222,"lapsityyppi":"funktiokutsu","tulosTunniste":null,"tulosTekstiFi":null,"tulosTekstiSv":null,"tulosTekstiEn":null,"tallennaTulos":false},"indeksi":1},{"lapsi":{"funktionimi":"KESKIARVO","arvokonvertteriparametrit":[],"arvovalikonvertteriparametrit":[],"syoteparametrit":[],"funktioargumentit":[{"lapsi":{"funktionimi":"HAELUKUARVO","arvokonvertteriparametrit":[],"arvovalikonvertteriparametrit":[],"syoteparametrit":[],"funktioargumentit":[],"valintaperusteviitteet":[{"tunniste":"PK_A1_VAL1","kuvaus":"","lahde":"HAETTAVA_ARVO","onPakollinen":false,"epasuoraViittaus":false,"indeksi":2}],"validointivirheet":[],"onLuonnos":null,"nimi":null,"kuvaus":null,"tyyppi":null,"id":819224,"lapsityyppi":"funktiokutsu","tulosTunniste":null,"tulosTekstiFi":null,"tulosTekstiSv":null,"tulosTekstiEn":null,"tallennaTulos":false},"indeksi":1},{"lapsi":{"funktionimi":"HAELUKUARVO","arvokonvertteriparametrit":[],"arvovalikonvertteriparametrit":[],"syoteparametrit":[],"funktioargumentit":[],"valintaperusteviitteet":[{"tunniste":"PK_A1_VAL2","kuvaus":"","lahde":"HAETTAVA_ARVO","onPakollinen":false,"epasuoraViittaus":false,"indeksi":2}],"validointivirheet":[],"onLuonnos":null,"nimi":null,"kuvaus":null,"tyyppi":null,"id":819226,"lapsityyppi":"funktiokutsu","tulosTunniste":null,"tulosTekstiFi":null,"tulosTekstiSv":null,"tulosTekstiEn":null,"tallennaTulos":false},"indeksi":2}],"valintaperusteviitteet":[],"validointivirheet":[],"onLuonnos":null,"nimi":null,"kuvaus":null,"tyyppi":null,"id":819228,"lapsityyppi":"funktiokutsu","tulosTunniste":null,"tulosTekstiFi":null,"tulosTekstiSv":null,"tulosTekstiEn":null,"tallennaTulos":false},"indeksi":2}],"valintaperusteviitteet":[],"validointivirheet":[],"onLuonnos":null,"nimi":null,"kuvaus":null,"tyyppi":null,"id":819231,"lapsityyppi":"funktiokutsu","tulosTunniste":null,"tulosTekstiFi":null,"tulosTekstiSv":null,"tulosTekstiEn":null,"tallennaTulos":false},"indeksi":1};
+        beforeEach(function () {
+            var funktiokutsu = {"lapsi": {"funktionimi": "KESKIARVO", "arvokonvertteriparametrit": [], "arvovalikonvertteriparametrit": [], "syoteparametrit": [], "funktioargumentit": [
+                {"lapsi": {"funktionimi": "HAELUKUARVO", "arvokonvertteriparametrit": [], "arvovalikonvertteriparametrit": [], "syoteparametrit": [], "funktioargumentit": [], "valintaperusteviitteet": [
+                    {"tunniste": "PK_A1", "kuvaus": "", "lahde": "HAETTAVA_ARVO", "onPakollinen": false, "epasuoraViittaus": false, "indeksi": 2}
+                ], "validointivirheet": [], "onLuonnos": null, "nimi": null, "kuvaus": null, "tyyppi": null, "id": 819222, "lapsityyppi": "funktiokutsu", "tulosTunniste": null, "tulosTekstiFi": null, "tulosTekstiSv": null, "tulosTekstiEn": null, "tallennaTulos": false}, "indeksi": 1},
+                {"lapsi": {"funktionimi": "KESKIARVO", "arvokonvertteriparametrit": [], "arvovalikonvertteriparametrit": [], "syoteparametrit": [], "funktioargumentit": [
+                    {"lapsi": {"funktionimi": "HAELUKUARVO", "arvokonvertteriparametrit": [], "arvovalikonvertteriparametrit": [], "syoteparametrit": [], "funktioargumentit": [], "valintaperusteviitteet": [
+                        {"tunniste": "PK_A1_VAL1", "kuvaus": "", "lahde": "HAETTAVA_ARVO", "onPakollinen": false, "epasuoraViittaus": false, "indeksi": 2}
+                    ], "validointivirheet": [], "onLuonnos": null, "nimi": null, "kuvaus": null, "tyyppi": null, "id": 819224, "lapsityyppi": "funktiokutsu", "tulosTunniste": null, "tulosTekstiFi": null, "tulosTekstiSv": null, "tulosTekstiEn": null, "tallennaTulos": false}, "indeksi": 1},
+                    {"lapsi": {"funktionimi": "HAELUKUARVO", "arvokonvertteriparametrit": [], "arvovalikonvertteriparametrit": [], "syoteparametrit": [], "funktioargumentit": [], "valintaperusteviitteet": [
+                        {"tunniste": "PK_A1_VAL2", "kuvaus": "", "lahde": "HAETTAVA_ARVO", "onPakollinen": false, "epasuoraViittaus": false, "indeksi": 2}
+                    ], "validointivirheet": [], "onLuonnos": null, "nimi": null, "kuvaus": null, "tyyppi": null, "id": 819226, "lapsityyppi": "funktiokutsu", "tulosTunniste": null, "tulosTekstiFi": null, "tulosTekstiSv": null, "tulosTekstiEn": null, "tallennaTulos": false}, "indeksi": 2}
+                ], "valintaperusteviitteet": [], "validointivirheet": [], "onLuonnos": null, "nimi": null, "kuvaus": null, "tyyppi": null, "id": 819228, "lapsityyppi": "funktiokutsu", "tulosTunniste": null, "tulosTekstiFi": null, "tulosTekstiSv": null, "tulosTekstiEn": null, "tallennaTulos": false}, "indeksi": 2}
+            ], "valintaperusteviitteet": [], "validointivirheet": [], "onLuonnos": null, "nimi": null, "kuvaus": null, "tyyppi": null, "id": 819231, "lapsityyppi": "funktiokutsu", "tulosTunniste": null, "tulosTekstiFi": null, "tulosTekstiSv": null, "tulosTekstiEn": null, "tallennaTulos": false}, "indeksi": 1};
             var controller = createController();
             $routeParams.laskentakaavaOid = 12345;
             $httpBackend.resetExpectations();
         });
+
+        afterEach(function() {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
 
         it("funktioasetukset initial values", function () {
             expect(scope.funktioasetukset.selectedFunktioIndex).toBe(undefined);
@@ -53,35 +71,33 @@ describe('LaskentakaavaController', function () {
             expect(scope.funktioasetukset.parentFunktiokutsu).toBeUndefined();
         });
 
-        it("controller initial values", function() {
+        it("controller initial values", function () {
             expect(scope.model).toEqual({});
         });
 
         //valintaperusteet -moduulissa
-        it("buildversion is fetched",function() {
+        it("buildversion is fetched", function () {
             $httpBackend.expectGET('buildversion.txt?auth');
+            $httpBackend.flush();
+
         });
 
-        it("Funktiokuvaukset are fetched",function() {
+        it("Funktiokuvaukset are fetched", function () {
             $httpBackend.expectGET('resources/laskentakaava/funktiokuvaus');
+            $httpBackend.flush();
         });
 
         it("Laskentakaava is fetched", function () {
             $httpBackend.expectGET('resources/laskentakaava/:oid');
+            $httpBackend.flush();
         });
 
 
-        /*
-        it("LaskentakaavaList has been reloaded", function() {
-            spyOn(scope, 'reloadLaskentakaavaLista');
-            expect(scope.reloadLaskentakaavaLista).toHaveBeenCalled();
-        });
-*/
+
 
 
 
     });
-
 
 
 });
