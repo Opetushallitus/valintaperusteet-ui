b25db047a4676b9c0662ff1490ea3c8e92bd9c85
Kalle Manninen <kalle.manninen@gofore.com>
2014-08-26 14:39:20 +0300

    VT-909 AngularJS Unit ja E2E testien luominen

--- src/test/ui/e2e/scenarios.js
+++ src/test/ui/e2e/scenarios.js
@@ -16,7 +16,7 @@ describe('E2E-TESTS', function() {
             });
 
             it('should select Ammatillisen item from dropdown', function() {
-                element(by.cssContainingText('option', 'Ammatillisen')).click();
+                element.all(by.cssContainingText('option', 'Ammatillisen')).first().click();
             });
 
 
@@ -33,12 +33,19 @@ describe('E2E-TESTS', function() {
                 input.click();
                 input = element(by.model('domain.search.vainHakukohteet'));
                 input.click();
+                input = element(by.model('domain.search.vainHakukohteet'));
+                input.click();
             });
 
 
         });
 
         describe('add Valintaryhmä', function() {
+            var item = by.xpath("//a[contains(@class, 'btn')]");
+            browser.wait(function() {
+                return browser.driver.isElementPresent(item);
+            }, 8000);
+
             it('should click add Valintaryhmä-button', function() {
                 element.all(by.xpath("//a[contains(@class, 'btn')]")).first().click();
             });
@@ -54,9 +61,104 @@ describe('E2E-TESTS', function() {
 
                 var item = element.all(by.model('model.parentOid')).last();
                 item.click();
+
+                element.all(by.model('model.parentOid')).first().click();
             });
 
+            it('should input text to Nimi-field', function() {
+                var input = element(by.model('model.valintaryhma.nimi'));
+                input.sendKeys('TestiValintaryhma');
+                expect(input.getAttribute('value')).toBe('TestiValintaryhma');
 
+            });
+
+            it('should click Tallenna-button', function() {
+                element(by.buttonText('Tallenna')).click();
+
+            });
+
+            it('should click Takaisin-button', function() {
+                element(by.buttonText('Takaisin')).click();
+
+            });
+        });
+
+        describe('edit Valintaryhmä', function() {
+
+            it('should be select new Valintaryhmä', function() {
+                element.all(by.xpath("//a[contains(text(),'TestiValintaryhma')]")).first().click();
+            });
+
+            it('should be add Valintaryhmä-page', function() {
+                expect(browser.getLocationAbsUrl()).toMatch("/valintaryhma");
+            });
+
+            it('should input text to Nimi-field', function() {
+                var input = element(by.model('model.valintaryhma.nimi'));
+                input.sendKeys('2');
+                expect(input.getAttribute('value')).toBe('TestiValintaryhma2');
+
+            });
+            it('should select item from Haku dropdown', function() {
+                element.all(by.model('model.valintaryhma.hakuoid')).first().click();
+            });
+            it('should select item from Kohdejoukko dropdown', function() {
+                element.all(by.model('model.valintaryhma.kohdejoukko')).last().click();
+            });
+
+            it('should click Organisaatio-button', function() {
+                element(by.buttonText('Organisaatio')).click();
+
+            });
+
+            it('should input text to search-field', function() {
+                var input = element(by.model('orgTree.searchStr'));
+                input.sendKeys('akaa');
+                expect(input.getAttribute('value')).toBe('akaa');
+
+            });
+
+            it('should be select Akaan opisto', function() {
+                var item = by.xpath("//a[contains(text(),'Akaan Opisto')]");
+                browser.wait(function() {
+                    return browser.driver.isElementPresent(item);
+                }, 8000);
+                element.all(item).first().click();
+                element.all(by.xpath("//a[contains(text(),'Sulje')]")).first().click();
+
+            });
+
+            it('should click Tallenna-button', function() {
+                var item = by.xpath("//button[contains(text(),'Tallenna')]");
+                browser.wait(function() {
+                    return browser.driver.isElementPresent(item);
+                }, 8000);
+                element(by.buttonText('Tallenna')).click();
+
+            });
+            it('should click Takaisin-button', function() {
+                element(by.buttonText('Takaisin')).click();
+
+            });
+        });
+
+        describe('delete Valintaryhmä', function() {
+
+            it('should be select new Valintaryhmä', function() {
+                element.all(by.xpath("//a[contains(text(),'TestiValintaryhma2')]")).first().click();
+            });
+
+            it('should be add Valintaryhmä-page', function() {
+                expect(browser.getLocationAbsUrl()).toMatch("/valintaryhma");
+            });
+            it('should click Poista-button', function() {
+                element(by.buttonText('Poista')).click();
+
+            });
+            it('should click Kyllä-button', function() {
+                element(by.buttonText('Kyllä')).click();
+
+            });
         });
     });
 });
