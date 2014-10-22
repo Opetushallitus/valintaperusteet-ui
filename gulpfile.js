62a5105c8270272c545bf5479c90990c6aec8228
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-10-22 10:20:50 +0300

    jquery.min.map polku lisätty gulpissa js-kirjastojen polkuihin

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ var paths = {
 
         'bower_components/jquery/dist/jquery.js',
         'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/jquery/dist/jquery.min.js.map',
+        'bower_components/jquery/dist/jquery.min.map',
 
         'bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
         'bower_components/jquery.i18n.properites/jquery.i18n.properties.js',
