00bf33dbad818f500b6525ec8ae55abb234c6393
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-12-09 17:21:50 +0200

    Test valintaryhma copy modal and error handling

--- src/test/webapp/test/page/valintaryhmaPage.js
+++ src/test/webapp/test/page/valintaryhmaPage.js
@@ -7,6 +7,21 @@ function valintaryhmaPage() {
         kopioiRyhma: function() {
             return S("a:contains(Valintaryhmän kopiointi)");
         },
+        kopioiButton: Button(function() {
+            return S("button:contains(Kopioi)");
+        }),
+        virheIlmoitus: function() {
+            return S("div.alert h3");
+        },
+        kopiointiOtsikko: function() {
+            return S("h3:contains(Valintaryhmän kopiointi)");
+        },
+        ryhmanUusinimi: function() {
+            return S("input#uusinimi");
+        },
+        juuriRyhma: function() {
+            return S("input#root");
+        },
         openPage: function (done) {
             openValintaryhmaPage().then(wait.until(function () {
               var pageReady = pageFunctions.kopioiRyhma().length === 1;
