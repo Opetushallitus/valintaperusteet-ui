81e19c1b2d59e813cafd92fabf20744b484b2a6f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-11-13 10:17:04 +0200

    Formatoi
    Ei toiminnallisia muutoksia.

--- src/test/java/fi/vm/sade/valintaperusteet/mocha/ValintaperusteetUIMochaTest.java
+++ src/test/java/fi/vm/sade/valintaperusteet/mocha/ValintaperusteetUIMochaTest.java
@@ -14,7 +14,12 @@ public class ValintaperusteetUIMochaTest {
     @Test
     public void mochaTest() throws Exception {
         JettyTestRunner.start();
-        String[] cmd = {"node_modules/phantomjs-prebuilt/bin/phantomjs", "node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js", "http://localhost:" + JettyTestRunner.PORT + "/valintaperusteet-ui/test/runner.html", "spec"};
+        String[] cmd = {
+            "node_modules/phantomjs-prebuilt/bin/phantomjs",
+            "node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js",
+            "http://localhost:" + JettyTestRunner.PORT + "/valintaperusteet-ui/test/runner.html",
+            "spec"
+        };
 
         ProcessBuilder pb = new ProcessBuilder(cmd);
         pb.inheritIO();
